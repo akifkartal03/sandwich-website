1449c042022f7773973960ee0d7b8814f8ad0ab0
mustaaftokgoz <mustafatokgz@icloud.com>
2020-12-06 21:49:16 +0300

    adding names of small images

--- src/components/RecipePage/Recipe.jsx
+++ src/components/RecipePage/Recipe.jsx
@@ -46,6 +46,7 @@ const Recipe = props => {
     var random = parseInt(Math.random() * all_recipies.length);
     var img1, img2, img3;
     var link1, link2, link3;
+    var name1, name2, name3;
     {
         if (
             all_recipies &&
@@ -55,6 +56,7 @@ const Recipe = props => {
         ) {
             img1 = all_recipies[random].imgURL;
             link1 = all_recipies[random]._id;
+            name1 = all_recipies[random].name;
             random = (random+1)%all_recipies.length;
             if (
                 all_recipies &&
@@ -64,6 +66,7 @@ const Recipe = props => {
             ) {
                 img2 = all_recipies[random].imgURL;
                 link2 = all_recipies[random]._id;
+                name2 = all_recipies[random].name;
             }
             random = (random+1)%all_recipies.length;
             if (
@@ -74,6 +77,7 @@ const Recipe = props => {
             ) {
                 img3 = all_recipies[random].imgURL;
                 link3 = all_recipies[random]._id;
+                name3 = all_recipies[random].name;
             }
         }
     }
@@ -104,23 +108,26 @@ const Recipe = props => {
                 </div>
             </div>
             <div className="Recipe-directions">
-                <h3 style={{ color: 'red', fontWeight: 'bolder' }}> Directions </h3>
+                <h3 style={{ color: '#af1507', fontWeight: 'bolder' }}> Directions </h3>
                 <p>{recipe.directions}</p>
             </div>
             <div className="Recipe">
                 <div id="imagesmall">
                     <a href={`/recipe/${link1}`}>
                         <img src={img1} height="150px" width="150px" />
+                        <p className="small-image-name">{name1}</p>
                     </a>
                 </div>
                 <div id="imagesmall">
                     <a href={`/recipe/${link2}`}>
                         <img src={img2} height="150px" width="150px" />
+                        <p className="small-image-name">{name2}</p>
                     </a>
                 </div>
                 <div id="imagesmall">
                     <a href={`/recipe/${link3}`}>
                         <img src={img3} height="150px" width="150px" />
+                        <p className="small-image-name">{name3}</p>
                     </a>
                 </div>
             </div>
