2c4755b03e37431c80fcb39b1c055943de4ddcee
MehdiKurtcebe <m.kurtcebe2018@gtu.edu.tr>
2020-12-08 23:49:09 +0300

    new test added to AllRecipies.test.js

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
-import App from './App';
+import App from '../../App';
 
 it('Website renders without crashing', () => {
   const div = document.createElement('div');
