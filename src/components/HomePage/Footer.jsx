16b6c2b515b6898808d473b42cbc0b68e845e16f
DjuroRad <djuro2017@gtu.edu.tr>
2020-12-17 23:19:59 +0100

    new tests

--- src/components/HomePage/Footer.jsx
+++ src/components/HomePage/Footer.jsx
@@ -5,9 +5,6 @@ const Footer = (props) => {
     return (
         <footer className="text-muted">
             <Container>
-                <p className="float-right">
-                    <a href="/">Back to top</a>
-                </p>
                 <p>Copyright to &copy; Sandwich</p>
                 <p>
                     Project's Github available on: 
