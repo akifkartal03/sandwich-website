cf342b679204f439d816ea8c804d3efd0c94090f
Akif Kartal <akokartal@gmail.com>
2020-12-26 13:54:24 +0300

    login page converted to hooks

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -1,5 +1,5 @@
 import './LoginPage.css';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Message from './Massage';
 import Error from './Error';
@@ -12,111 +12,92 @@ import {
     LOGIN_MESSAGE,
     ERROR_IN_LOGIN
 } from './MassageBundle';
-class LoginPage extends Component {
-    constructor(props) {
-        super(props);
+const LoginPage = () => {
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(false);
+    const [loginSuccess, setloginSuccess] = useState(false);
 
-        this.state = {
-            user_name: '',
-            password: '',
-            error: false,
-            loginSuccess: false
-        };
-    }
-
-    handleOnChangeUserName = e => {
-        this.setState({
-            user_name: e.target.value
-        });
+    const handleOnChangeUserName = e => {
+        setUserName(e.target.value);
     };
-
-    handleOnChangePassword = e => {
-        this.setState({
-            password: e.target.value
-        });
+    const handleOnChangePassword = e => {
+        setPassword(e.target.value);
     };
-
-    onSubmit = async e => {
+    const onSubmit = async e => {
         var loginResult = false;
-        UserDataService.getByUsername(this.state.user_name)
+        UserDataService.getByUsername(username)
             .then(response => {
                 if (response.data !== null) {
-                    if (response.data.password === this.state.password) {
+                    if (response.data.password === password) {
                         loginResult = true;
                     }
                 }
                 if (loginResult !== true) {
-                    this.setState({
-                        error: true,
-                        loginSuccess: false
-                    });
-                } else
-                    this.setState({
-                        loginSuccess: true,
-                        error: false
-                    });
+                    setError(true);
+                    setloginSuccess(false);
+                } else {
+                    setError(false);
+                    setloginSuccess(true);
+                }
             })
             .catch(e => {
                 console.log(e);
             });
     };
-
-    render() {
-        const { loginSuccess, error } = this.state;
-        return (
-            <div className="Login">
-                <div className="loginImage">
-                    <img
-                        src={loginImg}
-                        width="300"
-                        style={{ position: 'relative', paddingTop: 5 }}
-                        alt="login"
-                    />
-                </div>
-                <form onSubmit={this.onSubmit}>
-                    <div>
-                        <div className="fields">
-                            <br />
-                            <p> {COMMON_FIELDS.USER_NAME} </p>{' '}
-                            <input
-                                type="text"
-                                name="Username"
-                                onChange={this.handleOnChangeUserName}
-                                autoComplete="Username"
-                                required
-                            />
-                        </div>
-                        <div className="fields">
-                            <br />
-                            <p> {COMMON_FIELDS.PASSWORD} </p>
-                            <input
-                                type="password"
-                                name="Password"
-                                onChange={this.handleOnChangePassword}
-                                autoComplete="Password"
-                                required
-                            />
-                        </div>
-
-                        <br />
-                        <div className="buttons">
-                            <button
-                                type="button"
-                                onClick={this.onSubmit}
-                                className="btn btn-primary"
-                            >
-                                {LOGIN_FIELDS.LOGIN}
-                            </button>
-                            {'    '}
-                            <Link to="/">{REGISTRATION_FIELDS.REGISTER} </Link>
-                        </div>
-                    </div>
-                </form>
-                <br />
-                {loginSuccess && <Message message={LOGIN_MESSAGE} />}
-                {error && <Error message={ERROR_IN_LOGIN} />}
+    return (
+        <div className="Login">
+            <div className="loginImage">
+                <img
+                    src={loginImg}
+                    width="300"
+                    style={{ position: 'relative', paddingTop: 5 }}
+                    alt="login"
+                />
             </div>
-        );
-    }
-}
+            <form onSubmit={onSubmit}>
+                <div>
+                    <div className="fields">
+                        <br />
+                        <p> {COMMON_FIELDS.USER_NAME} </p>{' '}
+                        <input
+                            type="text"
+                            name="Username"
+                            onChange={handleOnChangeUserName}
+                            autoComplete="Username"
+                            required
+                        />
+                    </div>
+                    <div className="fields">
+                        <br />
+                        <p> {COMMON_FIELDS.PASSWORD} </p>
+                        <input
+                            type="password"
+                            name="Password"
+                            onChange={handleOnChangePassword}
+                            autoComplete="Password"
+                            required
+                        />
+                    </div>
+
+                    <br />
+                    <div className="buttons">
+                        <button
+                            type="button"
+                            onClick={onSubmit}
+                            className="btn btn-primary"
+                        >
+                            {LOGIN_FIELDS.LOGIN}
+                        </button>
+                        {'    '}
+                        <Link to="/">{REGISTRATION_FIELDS.REGISTER} </Link>
+                    </div>
+                </div>
+            </form>
+            <br />
+            {loginSuccess && <Message message={LOGIN_MESSAGE} />}
+            {error && <Error message={ERROR_IN_LOGIN} />}
+        </div>
+    );
+};
 export default LoginPage;
