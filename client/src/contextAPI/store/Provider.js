3047a6ba44457f95427bb09112855f5a6aee9b02
Akif Kartal <akokartal@gmail.com>
2021-01-28 13:57:29 +0300

    v2.0 release

--- client/src/contextAPI/store/Provider.js
+++ client/src/contextAPI/store/Provider.js
@@ -1,6 +1,6 @@
 import React, { createContext, useReducer,useContext } from "react";
 
-const StateContext = createContext();
+export const StateContext = createContext([[],() => {}]);
 export function StateProvider({ children,reducer,initialValue }) {
   return <StateContext.Provider value={useReducer(reducer,initialValue)}>{children}</StateContext.Provider>;
 }
