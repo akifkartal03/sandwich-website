d51398b5479f833f00b90b4350a065e89a6d4281
akifkartal03 <akokartal@gmail.com>
2021-08-30 16:22:16 +0300

    update api

---
+++ client/src/components/HomePage/Api.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Row } from "reactstrap";
+const Api = (props) => {
+  return (
+    <div
+      style={{
+        paddingLeft: 50,
+        paddingTop: 15,
+      }}
+    >
+      <Row>
+          <h1>Welcome to CRM API</h1>
+      </Row>
+      <Row>
+          <h5>Some Services are following</h5>
+      </Row>
+      <Row>
+          <p>https://crm5api.herokuapp.com/api/v1/GetCustomerChannelCampaignList</p>
+      </Row>
+      <Row>
+          <p>https://crm5api.herokuapp.com/api/v1/GetCustomerDefinedCampaigns/CostumerId</p>
+      </Row>
+    </div>
+  );
+};
+
+export default Api;
