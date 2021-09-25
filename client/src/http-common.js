495313c06c8ad1ea9c1bc92f0caeb5ef929d87ef
akifkartal03 <akokartal@gmail.com>
2021-09-25 10:54:51 +0300

    add last changes

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 export default axios.create({
-    baseURL: "https://crm5api.herokuapp.com/"
-  });
+    baseURL: 'https://sandwichrw.herokuapp.com/',
+});
